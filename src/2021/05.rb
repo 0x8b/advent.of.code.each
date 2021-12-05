7ddee3750e5b56cb516558cd3417d3da0fb8e401
Mariusz Gumienny <mkgumienny@gmail.com>
2021-12-05 08:21:35 +0100

    .

---
+++ src/2021/05.rb
@@ -0,0 +1,24 @@
+SEGMENTS = ARGF.read.lines.map { _1.scan(/\d+/).map &:to_i }
+
+def points s
+  x1, y1, x2, y2 = s
+
+  dx = x2 <=> x1
+  dy = y2 <=> y1
+
+  if dx == 0
+    y1.step(y2, dy).map { [x1, _1] }
+  elsif dy == 0
+    x1.step(x2, dx).map { [_1, y1] }
+  else
+    x1.step(x2, dx).zip(y1.step(y2, dy))
+  end
+end
+
+def count_overlapping_points segments
+  segments.flat_map { |s| points(s) }.tally.values.count { _1 > 1 }
+end
+
+
+p count_overlapping_points SEGMENTS.select { |x1, y1, x2, y2| x1 == x2 or y1 == y2 }
+p count_overlapping_points SEGMENTS
