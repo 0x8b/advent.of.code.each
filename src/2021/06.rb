d5875da6a4e3d800fbf2b860a6f9765fa2f229dd
Mariusz Gumienny <mkgumienny@gmail.com>
2021-12-06 07:23:54 +0100

    .

--- src/2021/06.rb
+++ src/2021/06.rb
@@ -4,13 +4,15 @@ def lanternfish epochs
   state = LANTERNFISH.clone.tally
 
   epochs.times do
-    state = state.to_a.flat_map do |k, v|
-      k == 0 ? [[6, v], [8, v]] : [[k - 1, v]]
-    end.map do |ary|
-      [ary].to_h
-    end.inject do |s, update|
-      s.merge(update) do |_, v, u| v + u end
+    next_state = Hash.new 0
+
+    state.to_a.flat_map do |k, v|
+      k == 0 ? [[6, v], [8, v]] : [[k - 1, v]] # notice that 0 and 7 -> 6
+    end.each do |k, v|
+      next_state[k] += v
     end
+
+    state.replace next_state
   end
 
   state.values.sum
