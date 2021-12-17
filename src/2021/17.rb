cd99c0defd065c8174c903d0c8c0df8d24191764
Mariusz Gumienny <mkgumienny@gmail.com>
2021-12-17 09:24:20 +0100

    .

--- src/2021/17.rb
+++ src/2021/17.rb
@@ -24,13 +24,39 @@ end
 max_y = 0
 total = 0
 
-(0..TARGETX.end).to_a.product((TARGETY.begin..1000).to_a).each do |vx, vy|
-  candidate = test(vx, vy)
+xs = (0..TARGETX.end)
+ys = (TARGETY.begin..150) # might be higher than 150 for other input
 
-  if candidate
-    total += 1
-    max_y = [candidate, max_y].max if candidate > max_y
+ppm = []
+
+ys.each do |vy|
+  ppm << []
+  xs.each do |vx|
+    candidate = test(vx, vy)
+
+    if candidate
+      total += 1
+      max_y = [candidate, max_y].max if candidate > max_y
+    end
+
+    ppm.last << if candidate
+      [255, 0, 0]
+    elsif vx == 0
+      [0, 255, 0]
+    elsif vy < 0
+      [0, 0, 128]
+    elsif vy == 0
+      [255, 0, 255]
+    else
+      [0, 0, 255]
+    end
   end
+
+  ppm << ppm.pop.flatten.join(" ")
+end
+
+if ENV["PREVIEW"] == "true"
+  File.write "preview.ppm", ["P3", "#{xs.size} #{ys.size}", "255", ppm.reverse].join("\n")
 end
 
 puts max_y # part 1
