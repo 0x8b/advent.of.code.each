93398e8df8f2a5d0948f9e745700cf04da504331
Mariusz Gumienny <mkgumienny@gmail.com>
2021-12-19 17:40:04 +0100

    .

--- src/2021/19.rb
+++ src/2021/19.rb
@@ -18,7 +18,7 @@ end
 
 def yaw ((x, y, z))
   xy = 1i * Complex(x, y)
-  return xy.imag, xy.real, z
+  return xy.real, xy.imag, z
 end
 
 TRANSFORMATIONS = [
@@ -26,24 +26,77 @@ TRANSFORMATIONS = [
   -> (b) { yaw b },
   -> (b) { yaw yaw b },
   -> (b) { yaw yaw yaw b },
+
   -> (b) { roll b },
   -> (b) { yaw roll b },
   -> (b) { yaw yaw roll b },
   -> (b) { yaw yaw yaw roll b },
+
   -> (b) { roll roll b },
   -> (b) { yaw roll roll b },
   -> (b) { yaw yaw roll roll b },
   -> (b) { yaw yaw yaw roll roll b },
+
   -> (b) { roll roll roll b },
   -> (b) { yaw roll roll roll b },
   -> (b) { yaw yaw roll roll roll b },
   -> (b) { yaw yaw yaw roll roll roll b },
+
   -> (b) { pitch b },
   -> (b) { yaw pitch b },
   -> (b) { yaw yaw pitch b },
   -> (b) { yaw yaw yaw pitch b },
+
   -> (b) { pitch pitch pitch b },
   -> (b) { yaw pitch pitch pitch b },
   -> (b) { yaw yaw pitch pitch pitch b },
   -> (b) { yaw yaw yaw pitch pitch pitch b },
 ]
+
+
+def relative_position from, to
+  from.zip(to).map { |a, b| b - a }
+end
+
+require 'set'
+
+BEACONS = SCANNERS.delete(0).to_set
+SCANNERS_POSITIONS = [[0, 0]]
+
+OFFSET = 3000 # very important
+
+until SCANNERS.empty?
+  scores = []
+
+  SCANNERS.each do |scanner, beacons|
+    TRANSFORMATIONS.each.with_index do |transformation, ti|
+      moved = beacons.map { |b| transformation.call(b).map { |c| c + OFFSET } }
+
+      most_common_distance, count = BEACONS.to_a.product(moved).map do |b1, b2|
+        relative_position(b1, b2)
+      end.tally.max_by { |distance, count| count }
+
+      scores << [scanner, ti, most_common_distance.map{ |c| c - OFFSET }.map { |c| -c }, count]
+    end
+  end
+
+  scanner, transformation, most_common_distance, _ = scores.max_by(&:last)
+
+  SCANNERS_POSITIONS << most_common_distance
+
+  SCANNERS[scanner].map do |beacon|
+    TRANSFORMATIONS[transformation].call(beacon)
+  end.map do |beacon|
+    beacon.zip(most_common_distance).map(&:sum)
+  end.each do |beacon|
+    BEACONS << beacon
+  end
+
+  SCANNERS.delete(scanner)
+end
+
+puts BEACONS.size # part 1
+
+puts SCANNERS_POSITIONS.combination(2).map { |b1, b2| # part 2
+  relative_position(b1, b2).map(&:abs).sum
+}.max
