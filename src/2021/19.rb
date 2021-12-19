f5a1d31e8275090811e639a6c851d51541856d7c
Mariusz Gumienny <mkgumienny@gmail.com>
2021-12-19 21:02:58 +0100

    .

--- src/2021/19.rb
+++ src/2021/19.rb
@@ -1,6 +1,8 @@
+require 'set'
+
 MAP = ARGF.read.split("\n\n").map do |scanner|
-  scanner.lines.drop(1).map do |bacon|
-    bacon.chomp.split(",").map(&:to_i)
+  scanner.lines.drop(1).map do |beacon|
+    beacon.chomp.split(",").map(&:to_i)
   end
 end
 
@@ -53,30 +55,27 @@ TRANSFORMATIONS = [
   -> (b) { yaw yaw yaw pitch pitch pitch b },
 ]
 
-
 def relative_position from, to
   from.zip(to).map { |a, b| b - a }
 end
 
-require 'set'
-
 BEACONS = SCANNERS.delete(0).to_set
 SCANNERS_POSITIONS = [[0, 0]]
 
-OFFSET = 3000 # very important
+TRANSLATE = 3000
 
 until SCANNERS.empty?
   scores = []
 
   SCANNERS.each do |scanner, beacons|
-    TRANSFORMATIONS.each.with_index do |transformation, ti|
-      moved = beacons.map { |b| transformation.call(b).map { |c| c + OFFSET } }
+    TRANSFORMATIONS.each.with_index do |transformation, tindex|
+      translated = beacons.map { |b| transformation.call(b).map { |v| v + TRANSLATE } }
 
-      most_common_distance, count = BEACONS.to_a.product(moved).map do |b1, b2|
+      most_common_distance, count = BEACONS.to_a.product(translated).map do |b1, b2|
         relative_position(b1, b2)
       end.tally.max_by { |distance, count| count }
 
-      scores << [scanner, ti, most_common_distance.map{ |c| c - OFFSET }.map { |c| -c }, count]
+      scores << [scanner, tindex, most_common_distance.map { |v| v - TRANSLATE }.map { |v| -v }, count]
     end
   end
 
@@ -95,8 +94,8 @@ until SCANNERS.empty?
   SCANNERS.delete(scanner)
 end
 
-puts BEACONS.size # part 1
+puts BEACONS.size # part one
 
-puts SCANNERS_POSITIONS.combination(2).map { |b1, b2| # part 2
+puts SCANNERS_POSITIONS.combination(2).map { |b1, b2| # part two
   relative_position(b1, b2).map(&:abs).sum
 }.max
