6895256eb50e12188ecfed97d72c5791715085d8
Mariusz Gumienny <mkgumienny@gmail.com>
2021-12-25 12:15:37 +0100

    .

---
+++ src/2021/25.rb
@@ -0,0 +1,34 @@
+state = ARGF.read.lines.map { |line| line.chomp.chars }
+
+h = state.size
+w = state.first.size
+
+steps = 1
+
+loop do
+  newstate = state.map { |row|
+    r = (row + [row.first]).join.gsub(/>\./, '.>').chars
+
+    if row.first == ?.
+      r.drop(1).rotate(-1)
+    else
+      r.take(w)
+    end
+  }.transpose.map { |row|
+    r = (row + [row.first]).join.gsub(/v\./, '.v').chars
+
+    if row.first == ?.
+      r.drop(1).rotate(-1)
+    else
+      r.take(h)
+    end
+  }.transpose
+
+  break if newstate == state
+
+  steps += 1
+
+  state.replace newstate
+end
+
+puts steps # part one
