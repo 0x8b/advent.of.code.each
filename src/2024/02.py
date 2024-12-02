2b76127cb25de432c945271b78983e593d972f18
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-02 12:35:22 +0100

    refaktoryzacja 2024/02

--- src/2024/02.py
+++ src/2024/02.py
@@ -4,29 +4,24 @@ data = pathlib.Path("../../data/2024/02.txt").read_text(encoding="utf-8")
 reports = [list([int(n) for n in line.split()]) for line in data.strip().split("\n")]
 
 
-def is_increasing(seq, index_to_remove=None):
-    cloned_seq = [n for n in seq]
+def prepare_sequence(seq, skip_index=None):
+    return [n for i, n in enumerate(seq) if i != skip_index]
 
-    if index_to_remove is not None:
-        cloned_seq.pop(index_to_remove)
+
+def is_increasing(seq, skip_index=None):
+    cloned_seq = prepare_sequence(seq, skip_index)
 
     return cloned_seq == list(sorted(cloned_seq))
 
 
-def is_decreasing(seq, index_to_remove=None):
-    cloned_seq = [n for n in seq]
-
-    if index_to_remove is not None:
-        cloned_seq.pop(index_to_remove)
+def is_decreasing(seq, skip_index=None):
+    cloned_seq = prepare_sequence(seq, skip_index)
 
     return cloned_seq == list(reversed(sorted(cloned_seq)))
 
 
-def is_difference_within_range(seq, minimum, maximum, index_to_remove=None):
-    cloned_seq = [n for n in seq]
-
-    if index_to_remove is not None:
-        cloned_seq.pop(index_to_remove)
+def is_difference_within_range(seq, minimum, maximum, skip_index=None):
+    cloned_seq = prepare_sequence(seq, skip_index)
 
     for i in range(len(cloned_seq) - 1):
         if not (minimum <= abs(cloned_seq[i] - cloned_seq[i + 1]) <= maximum):
@@ -46,11 +41,12 @@ for report in reports:
     if (
         is_increasing(report)
         or is_decreasing(report)
-        or any(is_increasing(report, i) for i in range(len(report)))
-        or any(is_decreasing(report, i) for i in range(len(report)))
+        or any(is_increasing(report, skip_index=i) for i in range(len(report)))
+        or any(is_decreasing(report, skip_index=i) for i in range(len(report)))
     ):
         if is_difference_within_range(report, 1, 3) or any(
-            is_difference_within_range(report, 1, 3, i) for i in range(len(report))
+            is_difference_within_range(report, 1, 3, skip_index=i)
+            for i in range(len(report))
         ):
             part_2 += 1
 
