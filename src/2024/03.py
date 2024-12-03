15e1f65f2e56aa81cfc6a3ece532aebdf0e8abf7
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-03 07:30:00 +0100

    refaktoryzacja 2024/03

--- src/2024/03.py
+++ src/2024/03.py
@@ -7,36 +7,32 @@ lines = data.strip().split("\n")
 
 memory = "".join(lines)
 
-part_1 = sum(
-    [
-        int(pair[0]) * int(pair[1])
-        for pair in [
-            re.findall(r"\d+", mul)
-            for mul in re.findall(r"mul\(\d{1,3},\d{1,3}\)", memory)
-        ]
-    ]
-)
+
+def get_answer(memory, pattern):
+    enabled = True
+    answer = 0
+
+    for instr in re.findall(pattern, memory):
+        match instr:
+            case _ if instr.startswith("don't"):
+                enabled = False
+            case _ if instr.startswith("do"):
+                enabled = True
+            case _ if instr.startswith("mul"):
+                if enabled:
+                    answer += functools.reduce(
+                        lambda product, m: product * m,
+                        [int(s) for s in re.findall(r"\d+", instr)],
+                        1,
+                    )
+            case _:
+                raise ValueError(f"unsupported instruction {instr}")
+
+    return answer
+
+
+part_1 = get_answer(memory, r"mul\(\d{1,3},\d{1,3}\)")
+part_2 = get_answer(memory, r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
 
 print(part_1)
-
-
-enabled = True
-part_2 = 0
-
-for instr in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", memory):
-    match instr:
-        case _ if instr.startswith("don't"):
-            enabled = False
-        case _ if instr.startswith("do"):
-            enabled = True
-        case _ if instr.startswith("mul"):
-            if enabled:
-                part_2 += functools.reduce(
-                    lambda product, m: product * m,
-                    [int(s) for s in re.findall(r"\d+", instr)],
-                    1,
-                )
-        case _:
-            raise ValueError(f"nieobsługiwana instrukcja {instr}")
-
 print(part_2)
