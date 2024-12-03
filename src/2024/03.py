04e01e4c592d833246f77a5b31c6cc2fb9369beb
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-03 14:09:26 +0100

    refaktoryzacja 2024/03

--- src/2024/03.py
+++ src/2024/03.py
@@ -12,22 +12,23 @@ def get_answer(memory, pattern):
     enabled = True
     answer = 0
 
-    for instr in re.findall(pattern, memory):
-        if instr.startswith("don't"):
-            enabled = False
-        elif instr.startswith("do"):
-            enabled = True
-        elif instr.startswith("mul"):
-            if enabled:
-                answer += operator.mul(*[int(s) for s in re.findall(r"\d+", instr)])
-        else:
-            raise ValueError(f"unsupported instruction {instr}")
+    for instr in re.finditer(pattern, memory):
+        match [m for m in instr.groups() if m is not None]:
+            case ["do"]:
+                enabled = True
+            case ["don't"]:
+                enabled = False
+            case ["mul", a, b]:
+                if enabled:
+                    answer += int(a) * int(b)
+            case _:
+                raise ValueError(f"unsupported instruction {instr}")
 
     return answer
 
 
-part_1 = get_answer(memory, r"mul\(\d{1,3},\d{1,3}\)")
-part_2 = get_answer(memory, r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
+part_1 = get_answer(memory, r"(mul)\((\d{1,3}),(\d{1,3})\)")
+part_2 = get_answer(memory, r"(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)")
 
 print(part_1)
 print(part_2)
