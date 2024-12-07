662ad4b10162859eed61cd50bbd01d5a8dde547e
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-07 06:28:17 +0100

    2024/07

--- src/2024/07.py
+++ src/2024/07.py
@@ -1,3 +1,5 @@
+import itertools
+import operator
 import pathlib
 
 from utils import *
@@ -5,4 +7,33 @@ from utils import *
 data = pathlib.Path("../../data/2024/07.txt").read_text(encoding="utf-8")
 lines = data.strip().split("\n")
 
-print(lines)
+equations = [ints(line) for line in lines]
+
+
+def get_calibration_result(equations, operators):
+    calibration_result = 0
+
+    for [result, *operands] in equations:
+        for operator_sequence in itertools.product(operators, repeat=len(operands) - 1):
+            operands_copy = [o for o in operands]
+
+            partial_result = operands_copy.pop(0)
+
+            for op in operator_sequence:
+                partial_result = op(partial_result, operands_copy.pop(0))
+
+            if partial_result == result:
+                calibration_result += result
+                break
+
+    return calibration_result
+
+
+part_1 = get_calibration_result(equations, [operator.add, operator.mul])
+part_2 = get_calibration_result(
+    equations, [operator.add, operator.mul, lambda a, b: int(str(a) + str(b))]
+)
+
+
+print(part_1)
+print(part_2)
