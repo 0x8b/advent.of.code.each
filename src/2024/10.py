407988a4f7244a1ef8b0bb2bc0207901fa6b061b
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-10 10:06:06 +0100

    refaktoryzacja 2024/10

--- src/2024/10.py
+++ src/2024/10.py
@@ -17,27 +17,22 @@ def find_trailheads(topo):
     }
 
 
-def count_all_trails(topo, trailhead_row, trailhead_col):
+def get_all_trails(topo, trailhead_row, trailhead_col):
     rows = len(topo)
     cols = len(topo[0])
 
     trails = []
 
     def hike(row, col, height, trail):
-        if not (0 <= row < rows and 0 <= col < cols):
-            return 0
-
-        if topo[row][col] != height:
-            return 0
+        if not (0 <= row < rows and 0 <= col < cols) or topo[row][col] != height:
+            return
 
         if height == 9:
             trails.append(trail)
-            return 1
+            return
 
-        return sum(
+        for dr, dc in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
             hike(row + dr, col + dc, height + 1, trail + [(row + dr, col + dc)])
-            for dr, dc in [[-1, 0], [0, 1], [1, 0], [0, -1]]
-        )
 
     hike(trailhead_row, trailhead_col, 0, [(trailhead_row, trailhead_col)])
 
@@ -48,7 +43,7 @@ def count_trails(topo, distinct=False):
     count = 0
 
     for trailhead_row, trailhead_col in find_trailheads(topo):
-        trails = count_all_trails(topo, trailhead_row, trailhead_col)
+        trails = get_all_trails(topo, trailhead_row, trailhead_col)
 
         if distinct:
             count += len(set(trail[-1] for trail in trails))
