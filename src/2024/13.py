08e362da7198cf4a8112890f0d2cdfa2969dd5a7
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-13 09:00:20 +0100

    2024/13

--- src/2024/13.py
+++ src/2024/13.py
@@ -4,6 +4,7 @@ from functools import cache
 
 from utils import *
 
+
 data = pathlib.Path("../../data/2024/13.txt").read_text(encoding="utf-8")
 
 machines = [
@@ -16,7 +17,7 @@ tokens_for_b = 1
 
 
 @cache
-def find_cheapest_way(ax, ay, an, bx, by, bn, px, py):
+def find_cheapest_way_naive(ax, ay, an, bx, by, bn, px, py):
     if px < 0 or py < 0:
         return math.inf
 
@@ -24,17 +25,24 @@ def find_cheapest_way(ax, ay, an, bx, by, bn, px, py):
         return an * tokens_for_a + bn * tokens_for_b
 
     return min(
-        find_cheapest_way(ax, ay, an + 1, bx, by, bn, px - ax, py - ay),
-        find_cheapest_way(ax, ay, an, bx, by, bn + 1, px - bx, py - by),
+        find_cheapest_way_naive(ax, ay, an + 1, bx, by, bn, px - ax, py - ay),
+        find_cheapest_way_naive(ax, ay, an, bx, by, bn + 1, px - bx, py - by),
     )
 
 
 part_1 = 0
+part_2 = 0
 
 for i, [[ax, ay], [bx, by], [px, py]] in enumerate(machines, 1):
     print(f"{i}/{len(machines)}")
 
-    if (result := find_cheapest_way(ax, ay, 0, bx, by, 0, px, py)) != math.inf:
+    if result := find_cheapest_way_naive(ax, ay, 0, bx, by, 0, px, py):
         part_1 += result
 
+    # if result := find_cheapest_way_naive(
+    #     ax, ay, 0, bx, by, 0, px + 10000000000000, py + 10000000000000
+    # ):
+    #     part_2 += result
+
 print(part_1)
+print(part_2)
