1abe14027d1d8bcf274cf8d8bc364f486895878c
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-14 08:15:34 +0100

    refaktoryzacja 2024/14

--- src/2024/14.py
+++ src/2024/14.py
@@ -10,58 +10,59 @@ lines = data.strip().split("\n")
 
 WIDTH = 101
 HEIGHT = 103
+TILES_X = 120
+TILES_Y = 120
 
+Robot = namedtuple("Robot", ["px", "py", "vx", "vy"])
 
-Robot = namedtuple("Robot", ["id", "px", "py", "vx", "vy"])
+robots = [Robot(*ints(line)) for line in lines]
 
-robots = [Robot(i, *ints(line)) for i, line in enumerate(lines)]
-
-img = Image.new("RGB", (WIDTH * 120, HEIGHT * 120), "white")
-
+img = Image.new("RGB", (WIDTH * TILES_X, HEIGHT * TILES_Y), "white")
 pixels = img.load()
 
 elapsed = 0
 
-for y in range(0, 120):
-    for x in range(0, 120):
+for ty in range(0, TILES_Y):
+    for tx in range(0, TILES_X):
         for i, robot in enumerate(robots):
             nx = (robot.px + (robot.vx + WIDTH)) % WIDTH
             ny = (robot.py + (robot.vy + HEIGHT)) % HEIGHT
 
-            nrobot = Robot(robot.id, nx, ny, robot.vx, robot.vy)
-
-            robots[i] = nrobot
+            robots[i] = Robot(nx, ny, robot.vx, robot.vy)
 
         elapsed += 1
 
         if elapsed == 100:
             mx = WIDTH // 2
             my = HEIGHT // 2
+
             quadrants = [0] * 4
 
             for robot in robots:
-                match robot:
-                    case Robot(_, px, py, _, _) if px < mx and py < my:
-                        quadrants[0] += 1
-                    case Robot(_, px, py, _, _) if px < mx and py > my:
-                        quadrants[1] += 1
-                    case Robot(_, px, py, _, _) if px > mx and py < my:
-                        quadrants[2] += 1
-                    case Robot(_, px, py, _, _) if px > mx and py > my:
-                        quadrants[3] += 1
+                px = robot.px
+                py = robot.py
+
+                if px < mx and py < my:
+                    quadrants[0] += 1
+                elif px < mx and py > my:
+                    quadrants[1] += 1
+                elif px > mx and py < my:
+                    quadrants[2] += 1
+                elif px > mx and py > my:
+                    quadrants[3] += 1
 
             part_1 = reduce(lambda m, n: m * n, quadrants, 1)
 
             print(part_1)
 
-        part_2 = 7623  # manually calculated from the generated image
+        part_2 = 7623  # value manually calculated based on the generated image
 
         if elapsed == 7623:
-            for i in range(101):
-                for j in range(103):
-                    pixels[x * WIDTH + i, y * HEIGHT + j] = (0, 255, 0)
+            for xx in range(WIDTH):
+                for yy in range(HEIGHT):
+                    pixels[tx * WIDTH + xx, ty * HEIGHT + yy] = (0, 255, 0)
 
         for robot in robots:
-            pixels[x * WIDTH + robot.px, y * HEIGHT + robot.py] = (0, 0, 0)
+            pixels[tx * WIDTH + robot.px, ty * HEIGHT + robot.py] = (0, 0, 0)
 
 img.save("robots.png", "PNG")
