76b6ec91461d091970f5191e7070d96b1399c2c9
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-18 08:02:11 +0100

    refaktoryzacja 2024/18

--- src/2024/18.py
+++ src/2024/18.py
@@ -7,46 +7,41 @@ from utils import *
 data = pathlib.Path("../../data/2024/18.txt").read_text(encoding="utf-8")
 
 rows, cols = 71, 71
-
-
 grid = [["." for x in range(cols)] for y in range(rows)]
-
-first_bytes = 1024
-bytes = list(batched(ints(data), 2))
-
-for col, row in bytes[:first_bytes]:
-    grid[row][col] = "#"
 
 
 def shortest_path_length(grid):
     edges = []
+    rows, cols = len(grid), len(grid[0])
 
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == ".":
                 for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    if 0 <= row + dr < rows and 0 <= col + dc < cols:
-                        if grid[row + dr][col + dc] == ".":
-                            edges.append(((row, col), (row + dr, col + dc), 1))
+                    if (
+                        0 <= row + dr < rows
+                        and 0 <= col + dc < cols
+                        and grid[row + dr][col + dc] == "."
+                    ):
+                        edges.append(((row, col), (row + dr, col + dc)))
 
-    graph = networkx.Graph()
-
-    graph.add_weighted_edges_from(edges)
-
-    return networkx.shortest_path_length(graph, (0, 0), (rows - 1, cols - 1))
+    return networkx.shortest_path_length(
+        networkx.Graph(edges), (0, 0), (rows - 1, cols - 1)
+    )
 
 
-part_1 = shortest_path_length(grid)
+bytes = list(batched(ints(data), 2))
 
-print(part_1)
+for col, row in bytes[:1024]:
+    grid[row][col] = "#"
 
-for gcol, grow in bytes[1024:]:
-    grid[grow][gcol] = "#"
+print(shortest_path_length(grid))  # part_1
+
+for col, row in bytes[1024:]:
+    grid[row][col] = "#"
 
     try:
         shortest_path_length(grid)
-    except Exception:
-        part_2 = f"{gcol},{grow}"
-
-        print(part_2)
+    except networkx.NetworkXNoPath:
+        print(f"{col},{row}")  # part_2
         break
