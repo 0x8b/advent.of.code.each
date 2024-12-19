4e452b263a45923d6dac78bd223b837e0df0e0b9
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-19 10:30:06 +0100

    refaktoryzacja 2024/19

--- src/2024/19.py
+++ src/2024/19.py
@@ -6,32 +6,32 @@ from utils import *
 data = pathlib.Path("../../data/2024/19.txt").read_text(encoding="utf-8")
 patterns, desired_designs = data.strip().split("\n\n")
 
-patterns = tuple(pattern.strip() for pattern in patterns.split(", "))
-desired_designs = [line.strip() for line in desired_designs.strip().split("\n")]
+PATTERNS = tuple(pattern.strip() for pattern in patterns.split(", "))
+DESIRED_DESIGNS = [line.strip() for line in desired_designs.strip().split("\n")]
 
 
 @cache
-def count(pattern, patterns, design):
-    if design == pattern:
+def count(pattern, design):
+    if pattern == design:
         return 1
 
     next_design = design[len(pattern) :]
-    n = 0
+    c = 0
 
-    for next_pattern in patterns:
+    for next_pattern in PATTERNS:
         if next_design.startswith(next_pattern):
-            n += count(next_pattern, patterns, next_design)
+            c += count(next_pattern, next_design)
 
-    return n
+    return c
 
 
-part_1 = 0
-part_2 = 0
+part_1, part_2 = 0, 0
 
-for design in desired_designs:
-    if (n := count("", patterns, design)) > 0:
-        part_1 += 1
-        part_2 += n
+for design in DESIRED_DESIGNS:
+    c = count("", design)
+
+    part_1 += int(c > 0)
+    part_2 += c
 
 print(part_1)
 print(part_2)
