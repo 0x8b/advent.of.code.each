afec3b43b4076c2f3cc86920c3e6e15aae518cc0
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-23 07:09:41 +0100

    refaktoryzacja 2024/23

--- src/2024/23.py
+++ src/2024/23.py
@@ -1,49 +1,22 @@
 import pathlib
 import networkx
 
-from utils import *
-
 data = pathlib.Path("../../data/2024/23.txt").read_text(encoding="utf-8")
-connections = [set(line.split("-")) for line in data.strip().split("\n")]
-
-groups = set()
-
-for connection in connections:
-    a, b = list(connection)
-    connected = [c for c in connections if a in c or b in c]
-    not_seen = set()
-
-    for c in connected:
-        not_seen.update(c)
-
-    not_seen.discard(a)
-    not_seen.discard(b)
-
-    for host in not_seen:
-        if {a, host} in connected and {b, host} in connected:
-            groups.add(",".join(list(sorted([a, b, host]))))
-
-groups = list(groups)
-
+connections = [tuple(line.split("-")) for line in data.strip().split("\n")]
 
 part_1 = 0
+part_2 = ""
 
-for group in groups:
-    if "t" in [group[0], group[3], group[6]]:
-        part_1 += 1
+max_clique_size = 0
 
+for clique in networkx.enumerate_all_cliques(networkx.Graph(connections)):
+    if len(clique) == 3:
+        part_1 += any(host.startswith("t") for host in clique)
+
+    if len(clique) > max_clique_size:
+        max_clique_size = len(clique)
+
+        part_2 = ",".join(sorted(clique))
 
 print(part_1)
-
-
-graph = networkx.Graph([tuple(connection) for connection in connections])
-
-passwords = [
-    ",".join(sorted(clique)) for clique in networkx.find_cliques_recursive(graph)
-]
-
-passwords.sort(key=len)
-
-part_2 = passwords[-1]
-
 print(part_2)
