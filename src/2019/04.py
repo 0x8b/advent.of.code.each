eb7cab09531d28c6c9a0932851b96ce1b0b404bd
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-28 22:41:47 +0100

    2019/04

---
+++ src/2019/04.py
@@ -0,0 +1,18 @@
+import pathlib
+from collections import Counter
+
+
+data = pathlib.Path("../../data/2019/04.txt").read_text(encoding="utf-8")
+lines = data.strip().split("\n")
+
+start, stop = lines[0].split("-")
+
+part_1 = 0
+
+for password in range(int(start), int(stop) + 1):
+    if list(str(password)) == list(sorted(str(password))) and any(
+        t[0] == t[1] for t in zip(str(password), str(password)[1:])
+    ):
+        part_1 += 1
+
+print(part_1)
