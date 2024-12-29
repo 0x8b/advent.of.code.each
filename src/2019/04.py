14e946471fe25052abdc4d95889af890d378117c
Mariusz Gumienny <mkgumienny@gmail.com>
2024-12-29 09:18:00 +0100

    2019/04

--- src/2019/04.py
+++ src/2019/04.py
@@ -8,6 +8,7 @@ lines = data.strip().split("\n")
 start, stop = lines[0].split("-")
 
 part_1 = 0
+part_2 = 0
 
 for password in range(int(start), int(stop) + 1):
     if list(str(password)) == list(sorted(str(password))) and any(
@@ -15,4 +16,8 @@ for password in range(int(start), int(stop) + 1):
     ):
         part_1 += 1
 
+        if 2 in Counter(str(password)).values():
+            part_2 += 1
+
 print(part_1)
+print(part_2)
