8e7f895cdaf84a57512f25e92893bf3ffa2f369f
Mariusz Gumienny <mkgumienny@gmail.com>
2025-01-06 07:15:05 +0100

    2016/05

--- src/2016/05.py
+++ src/2016/05.py
@@ -1,3 +1,4 @@
+import hashlib
 import pathlib
 
 from utils import *
@@ -5,6 +6,45 @@ from utils import *
 data = pathlib.Path("../../data/2016/05.txt").read_text(encoding="utf-8")
 lines = data.strip().split("\n")
 
-password = lines[0]
+door_id = lines[0]
 
-print(password)
+password = ""
+i = 0
+
+while len(password) != 8:
+    h = hashlib.new("md5")
+    h.update(bytes(f"{door_id}{i}", "utf-8"))
+    digest = h.hexdigest()
+
+    if digest.startswith("00000") and digest[5] != "0":
+        password += digest[5]
+
+    i += 1
+
+part_1 = password
+
+print(part_1)
+
+
+password = [None] * 8
+i = 0
+
+while True:
+    h = hashlib.new("md5")
+    h.update(bytes(f"{door_id}{i}", "utf-8"))
+    digest = h.hexdigest()
+
+    if digest.startswith("00000") and digest[5].isdigit():
+        index = int(digest[5])
+
+        if 0 <= index < 8 and not password[index]:
+            password[index] = digest[6]
+
+        if None not in password:
+            break
+
+    i += 1
+
+part_2 = "".join(password)
+
+print(part_2)
