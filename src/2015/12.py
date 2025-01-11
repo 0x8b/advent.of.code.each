098e3dfc89704b45ce1b01df5fe2b59fe6df8ff4
Mariusz Gumienny <mkgumienny@gmail.com>
2025-01-11 10:38:56 +0100

    refaktoryzacja 2015/12

--- src/2015/12.py
+++ src/2015/12.py
@@ -29,3 +29,19 @@ def replace_red_dicts_with_zero(obj):
 part_2 = sum(ints(json.dumps(replace_red_dicts_with_zero(json.loads(json_structure)))))
 
 print(part_2)
+
+
+def sum_part_2(obj):
+    if isinstance(obj, int):
+        return obj
+    elif isinstance(obj, dict) and "red" not in obj.values():
+        return sum(sum_part_2(value) for value in obj.values())
+    elif isinstance(obj, list):
+        return sum(sum_part_2(value) for value in obj)
+    else:
+        return 0
+
+
+part_2 = sum_part_2(json.loads(json_structure))
+
+print(part_2)
