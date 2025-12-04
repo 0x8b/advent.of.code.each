08d5ef5270f88a5af890e877adb5132e767b8093
Mariusz Gumienny <mkgumienny@gmail.com>
2025-12-04 07:51:41 +0100

    2025/04 part 2

--- src/2025/04.py
+++ src/2025/04.py
@@ -29,3 +29,36 @@ for y in range(0, len(data)):
         part_1 += count < 4
 
 print(part_1)
+
+part_2 = 0
+
+while True:
+    deleted = False
+
+    for y in range(0, len(data)):
+        for x in range(0, len(data[0])):
+            count = 0
+
+            if data[y][x] != "@":
+                continue
+
+            for dx in (-1, 0, 1):
+                for dy in (-1, 0, 1):
+                    if dx == 0 and dy == 0:
+                        continue
+
+                    xx = x + dx
+                    yy = y + dy
+
+                    if 0 <= yy < len(data) and 0 <= xx < len(data[0]):
+                        count += data[yy][xx] == "@"
+
+            if count < 4:
+                deleted = True
+                data[y][x] = "x"
+                part_2 += 1
+
+    if deleted == False:
+        break
+
+print(part_2)
